fbb5782c6dc137ec76a8039d11eabf6537c9821f
Kenneth Saey <kenneth.saey@g-flux.com>
2013-06-10 16:42:05 +0200

    Commit after first file creation

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="" type="text/css" rel="stylesheet"/>
+        <script src="" type="text/javascript"></script>
+        <title></title>
+    </head>
+    <body>
+        Initial index page for the master branch of the test-repository.
+    </body>
+</html>
